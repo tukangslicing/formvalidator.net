67d1d0e4fbf1c80214211784a924d9ea04a3c740
Victor Jonsson <kontakt@victorjonsson.se>
2013-08-17 14:59:03 +0200

    updated to latest version of the plugin

--- validate-email.php
+++ validate-email.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
 echo json_encode(array(
     'valid' => isset($_REQUEST['code']) && $_REQUEST['code'] == 'secret',
     'message'=>'The value has to be &quot;secret&quot; (this message comes from backend)'
